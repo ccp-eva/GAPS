f576d0453e5034154e73b376b93565e954bc5926
Luke Maurits <luke@maurits.id.au>
2024-09-19 11:26:29 +0200

    Substantial updates to articles per child model fitting script.
    * Remove `mice` library based imputation now that we are imputing
      the data beforehand.
    * No longer need to subset out datasets based on NAs thanks to the
      the imputation.
    * Remove CFST models because we didn't impute that.
    * Remove all model comparison stuff, that will be added to a summary
      markdown document later.

--- analysis/fit_models_articles_per_child.R
+++ analysis/fit_models_articles_per_child.R
@@ -1,12 +1,11 @@
 library(tidyverse)
 library(brms)
-library(mice)
 
 d <- read_csv("../data/articles_per_child.csv")
 
 fit_and_save_single_predictor_model <- function(d, predictor) {
-  if(predictor == "western") {
-	  form <- "log_articles_per_child ~ 0 + western"
+  if(predictor %in% c("western", "weird")) {
+	  form <- paste("log_articles_per_child ~ 0 +", predictor)
   } else {
     form <- paste("log_articles_per_child ~ mo(", predictor, ")", sep="")
   }
@@ -17,105 +16,51 @@ fit_and_save_single_predictor_model <- function(d, predictor) {
   return(m)
 }
 
-# Nielsen WEIRD
+# Null model
 m_null <- brm(log_articles_per_child ~ 1,
 	  data=d, cores=4) %>%
           add_criterion("loo")
+saveRDS(m_null, "m_null.rds")
 
-m_nielsen <- brm(log_articles_per_child ~ weird,
-	  data=d, cores=4) %>%
-          add_criterion("loo")
-saveRDS(m_nielsen, "m_nielsen.rds")
+# Nielsen WEIRD
+m_nielsen <- fit_and_save_single_predictor_model(d, "weird")
 
-loo_compare(m_null, m_nielsen)
-model_weights(m_null, m_nielsen)
+# Separated WEIRD components
+m_w <- fit_and_save_single_predictor_model(d, "western")
+m_e <- fit_and_save_single_predictor_model(d, "education")
+m_i <- fit_and_save_single_predictor_model(d, "industry")
+m_r <- fit_and_save_single_predictor_model(d, "gdp")
+m_d <- fit_and_save_single_predictor_model(d, "democracy")
 
-# Cultural distance from US
-d_cfst <- d %>%
-	filter(!is.na(distUS))
-m_cfst <- brm(log_articles_per_child ~ mo(distUS),
-	  data=d_cfst, cores=4) %>%
-          add_criterion("loo")
-saveRDS(m_cfst, "m_cfst.rds")
+# Alternative components
+M_h <- fit_and_save_single_predictor_model(d, "household_size")
+M_u <- fit_and_save_single_predictor_model(d, "urbanism")
+M_g <- fit_and_save_single_predictor_model(d, "gini")
+M_r <- fit_and_save_single_predictor_model(d, "religiosity")
+M_f <- fit_and_save_single_predictor_model(d, "ethfrac")
 
-m_nielsen_sub <- brm(log_articles_per_child ~ weird,
-	  data=d_cfst, cores=4) %>%
-          add_criterion("loo")
-loo_compare(m_nielsen_sub, m_cfst)
-model_weights(m_nielsen_sub, m_cfst)
-
-# WEIRD shoot out
-d_weird <- d %>%
-	filter(!is.na(western),
-	       !is.na(education),
-	       !is.na(industry),
-	       !is.na(gdp),
-	       !is.na(democracy))
-
-m_w <- fit_and_save_single_predictor_model(d_weird, "western")
-m_e <- fit_and_save_single_predictor_model(d_weird, "education")
-m_i <- fit_and_save_single_predictor_model(d_weird, "industry")
-m_r <- fit_and_save_single_predictor_model(d_weird, "gdp")
-m_d <- fit_and_save_single_predictor_model(d_weird, "democracy")
-
-loo_compare(m_w, m_e, m_i, m_r, m_d)
-model_weights(m_w, m_e, m_i, m_r, m_d)
-
-# Theory shoot out
-d_theory <- d %>%
-	filter(!is.na(individualism),
-	       !is.na(urbanism),
-	       !is.na(household_size),
-	       !is.na(religiosity),
-	       !is.na(ethfrac),
-	       !is.na(gini))
-
-M_h <- fit_and_save_single_predictor_model(d_theory, "household_size")
-M_u <- fit_and_save_single_predictor_model(d_theory, "urbanism")
-M_g <- fit_and_save_single_predictor_model(d_theory, "gini")
-M_r <- fit_and_save_single_predictor_model(d_theory, "religiosity")
-M_i <- fit_and_save_single_predictor_model(d_theory, "individualism")
-
-loo_compare(M_h, M_u, M_g, M_r, M_i)
-model_weights(M_h, M_u, M_g, M_r, M_i)
-
-d_weird_imp <- d %>%
-	select(log_articles_per_child,
-	       western,
-	       education,
-	       industry,
-	       gdp,
-	       democracy) %>%
-	mice(m = 5, print = FALSE)
-
-m_weird <- brm_multiple(log_articles_per_child ~ western +
+# Combined WEIRD components
+m_weird <- brm(log_articles_per_child ~ western +
 	                          mo(education) +
 				  mo(industry) +
 				  mo(gdp) +
 				  mo(democracy),
-	  data=d_weird_imp, cores=4) %>%
+	  data=d, cores=4) %>%
           add_criterion("loo")
 saveRDS(m_weird, "m_multipred_weird.rds")
 
-d_theory_imp <- d %>%
-	select(log_articles_per_child,
-	       household_size,
-	       urbanism,
-	       gini,
-	       religiosity,
-	       ethfrac,
-	       individualism) %>%
-	mice(m = 5, print = FALSE)
-
-m_theory <- brm_multiple(log_articles_per_child ~ mo(individualism) +
+# Combined alternative components
+m_alt <- brm(log_articles_per_child ~ mo(household_size) +
             mo(urbanism) +
-            mo(household_size) +
+            mo(gini) +
             mo(religiosity) +
-            mo(ethfrac) +
-            mo(gini),
-	  data=d_theory_imp, cores=4) %>%
+            mo(ethfrac),
+	  data=d, cores=4) %>%
           add_criterion("loo")
-saveRDS(m_theory, "m_multipred_weird.rds")
+saveRDS(m_alt, "m_multipred_alt.rds")
 
-loo_compare(m_weird, m_theory)
-model_weights(m_weird, m_theory)
+m_mega <- brm(log_articles_per_child ~ western + mo(education) + mo(industry) + mo(gdp) + mo(democracy) +
+                                       mo(household_size) + mo(urbanism) + mo(gini) + mo(religiosity) + mo(ethfrac),
+	  data=d, cores=4) %>%
+          add_criterion("loo")
+saveRDS(m_mega, "m_multipred_all.rds")
