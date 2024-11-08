526174da0e6663b0531511b617ece95faa8f4ff9
Luke Maurits <luke@maurits.id.au>
2024-11-08 16:12:58 +0100

    Update citation models to not use WEIRD dichotomy.

--- analysis/fit_citation_models.R
+++ analysis/fit_citation_models.R
@@ -7,17 +7,28 @@ d$multi_country <- d$N_countries > 1
 
 # New prior for negative binomials
 shared_prior =prior(normal(5, 2.5), class="Intercept") +
-	      prior(normal(2.5, 1.25), class="b") +
+	      prior(normal(0, 2.5), class="b") +
 	      prior(exponential(5), class="sd")
 
 # First citation models with offset
-null_formula <- "citations ~ 1 + offset(log(exposure)) + mo(N_countries) +
-	                   (1 + mo(N_countries) | journal)"
+null_formula <- "citations ~ 1 + offset(log(exposure)) + multi_country +
+	                   (1 + multi_country | journal)"
 
 any_nonweird_formula <- "citations ~ 1 + offset(log(exposure)) + multi_country*any_nonweird +
 	                   (1 + multi_country*any_nonweird | journal)"
 
-m <- brm(null_formula, data=d, family="negbinomial", cores=4, prior=shared_prior, control=list(adapt_delta=0.9))
-saveRDS(m, "m_citations_null.rds")
-m <- brm(any_nonweird_formula, data=d, family="negbinomial", cores=4, prior=shared_prior, control=list(adapt_delta=0.9))
-saveRDS(m, "m_citations_any_nonweird.rds")
+
+m_null <- brm(bf(as.formula(null_formula), shape~multi_country), data=d, family="negbinomial", cores=4, prior=shared_prior, control=list(adapt_delta=0.9))
+saveRDS(m, "m_citations_null_shape.rds")
+
+singpred_formula <- "citations ~ 1 + offset(log(exposure)) +
+			multi_country + mo(predictor) +
+	           (1 + multi_country + mo(predictor) | journal)"
+predictors <- c("education", "industry", "gdp", "democracy", "household_size", "urbanism", "gini", "religiosity", "ethfrac")
+
+for(predictor in predictors) {
+		form_str <- str_replace_all(singpred_formula, "predictor", predictor)
+		formul <- as.formula(form_str)
+		m <- brm(formul, data=d, family="negbinomial", cores=4, prior = shared_prior)
+		saveRDS(m, paste("m_citations_singpred_", predictor, "_quick.rds", sep=""))
+}
--- analysis/plot_models_citations.R
+++ analysis/plot_models_citations.R
@@ -1,38 +1,131 @@
 #!/usr/bin/env Rscript
 library(tidyverse)
 library(brms)
+library(patchwork)
 
-m <- readRDS("m_citations_any_nonweird.rds")
-
-nd <- expand_grid(multi_country=c(T,F), any_nonweird=c(T,F), exposure=10)
-preds <- posterior_predict(m, newdata=nd, re_formula=NA)
-plot_table <- tibble(multi_country=logical(), any_nonweird=logical(), citations=integer())
-for(j in 1:10) {
-	for(i in 1:nrow(nd)) {
-		plot_table <- add_row(plot_table, multi_country=nd$multi_country[i], any_nonweird=nd$any_nonweird[i], citations=preds[,i])
+pretty_varname <- function(varname) {
+	if(varname == "education") {
+		return("Educated")
+	} else if(varname == "industry") {
+		return("Industrialized")
+	} else if(varname == "gdp") {
+		return("Rich")
+	} else if(varname == "democracy") {
+		return("Democractic")
+	} else if(varname == "gini") {
+		return("Econ. Unequal")
+	} else if(varname == "household_size") {
+		return("Household Size")
+	} else if(varname == "urbanism") {
+		return("Urbanized")
+	} else if(varname == "ethfrac") {
+		return("Ethnically Diverse")
+	} else if(varname == "religiosity") {
+		return("Religious")
+	} else {
+		return(str_to_title(varname))
 	}
 }
 
-plot_table <- plot_table %>%
-	mutate(type=case_when(!multi_country & !any_nonweird ~ "Single WEIRD",
-			      !multi_country & any_nonweird ~ "Single non-WEIRD",
-			      multi_country & !any_nonweird ~ "Multiple, all WEIRD",
-			      multi_country & any_nonweird ~ "Multiple, one or more non-WEIRD")) %>%
-	mutate(`Article type`=factor(type, c(
-			      "Single WEIRD",
-			      "Single non-WEIRD",
-			      "Multiple, all WEIRD",
-			      "Multiple, one or more non-WEIRD"
-			      )))
+predictors <- c("education", "industry", "gdp", "democracy",
+	"household_size", "urbanism", "gini", "religiosity", "ethfrac")
 
-p <- ggplot(plot_table) +
-	geom_density(aes(x=citations, group=`Article type`), fill="Grey", bw=7.5) +
-	geom_vline(aes(xintercept=mean, ), linetype="dotted", size=2, show.legend=FALSE, data=summarise(group_by(plot_table, `Article type`), mean=mean(citations))) +
-	facet_wrap(~`Article type`, dir="v", ncol=1) +
-	xlab("Citations 10 years after publication") +
+# Initialise model comparison table and add null model
+model_comparison_table <- tibble(predictor = character(), looic=numeric(), looic_se=numeric())
+nullmodel_filename <- paste("m_citations_null_shape.rds", sep="")
+m <- readRDS(nullmodel_filename)
+l <- loo(m, moment_match=FALSE)$estimates["looic",]
+model_comparison_table <- add_row(model_comparison_table, predictor = "Null model", looic=l[1], looic_se=l[2])
+
+legend =TRUE
+
+# Initialise plot list
+plotlist <- list()
+plot_index <- 1
+
+nice_title <- "(e) Citations"
+yaxis_label <- "Expected citations after 10 years"
+
+# Iterate through predictors,
+# updating model comparison table each time and
+# adding plots to plotlist
+for(j in 1:length(predictors)) {
+	predictor <- predictors[j]
+	model_filename <- paste("m_citations_singpred_", predictor, "_shape.rds", sep="")
+	if(file.exists(model_filename)) {
+		print(paste("found", model_filename))
+		m <- readRDS(model_filename)
+
+		# Update model comparison table
+		l <- loo(m, moment_match=FALSE)$estimates["looic",]
+		model_comparison_table <- add_row(model_comparison_table, predictor = pretty_varname(predictor), looic=l[1], looic_se=l[2])
+
+		# Build plot table
+		plot_table <- tibble(value=numeric(), multi_country=logical(), mean_outcome=numeric(), low_outcome=numeric(), high_outcome=numeric())
+		nd <- expand_grid(foo=1:10, multi_country=c(F, T), exposure=10)
+		names(nd) <- c(predictor, "multi_country", "exposure")
+		preds <- posterior_linpred(m, newdata=nd, transform=TRUE, re_formula=NA)
+		for(k in 1:nrow(nd)) {
+			plot_table <- add_row(plot_table, value=nd[[predictor]][k], multi_country=nd$multi_country[k], mean_outcome=mean(preds[,k]), low_outcome=quantile(preds[,k], p=0.025), high_outcome=quantile(preds[,k], p=0.975))
+		}
+		foonames <- colnames(plot_table)
+		foonames[1] <- predictor
+		colnames(plot_table) <- foonames
+
+		# Create plot, append to plotlist
+		predictor <- ensym(predictor)
+		plot_table$`Countries sampled` <- if_else(plot_table$multi_country, "Multiple", "Single")
+		plot_table$`Countries sampled` <- factor(plot_table$`Countries sampled`, c("Single", "Multiple"), ordered=TRUE)
+		p <- ggplot(plot_table) +
+			geom_line(aes(x=!!predictor, y=mean_outcome, group=`Countries sampled`, colour=`Countries sampled`), show.legend = legend && j == length(predictors)) +
+			geom_ribbon(aes(x=!!predictor, ymin=low_outcome, ymax=high_outcome, fill=`Countries sampled`, group=`Countries sampled`), alpha=0.25, show.legend = legend && j == length(predictors)) +
+#			geom_hline(yintercept=0.5, linetype="dotted") +
+
+			ggtitle(pretty_varname(predictor)) +
+			labs(fill="Countries\nsampled", colour="Countries\nsampled") +
+			xlab("Decile") +
+			ylab(yaxis_label) +
+
+			scale_x_continuous(breaks=1:10) +
+			scale_y_continuous(limits=c(0, 2000), position="right") +
+			scale_colour_manual(values=c("#F8766D", "#00BFC4")) +
+			scale_fill_manual(values=c("#F8766D", "#00BFC4")) +
+			theme_bw() +
+			coord_cartesian(ylim = c(0, 300)) +
+			theme(aspect.ratio = 1)
+		if(j != 9) {
+			p <- p + theme(axis.text.y=element_blank(), axis.title.y=element_blank())
+		}
+		plotlist[[plot_index]] <- p
+		plot_index <- plot_index + 1
+	} else {
+		print(paste("Ruh roh on ", predictor))
+	}
+}
+# Make mod comparison plot
+model_comparison_table <- model_comparison_table %>%
+	mutate(looic_1se_down = looic - 1*looic_se,
+	       looic_1se_up   = looic + 1*looic_se,
+	       looic_2se_down = looic - 2*looic_se,
+	       looic_2se_up   = looic + 2*looic_se)
+
+ranked_predictors <- model_comparison_table %>%
+	arrange(-looic) %>%
+	pull(predictor)
+model_comparison_table$predictor = factor(model_comparison_table$predictor, ranked_predictors)
+
+p <- ggplot(model_comparison_table) +
+	geom_segment(aes(x=looic_2se_down, xend=looic_2se_up, y=predictor, yend=predictor), size=2, alpha=0.5) +
+	geom_segment(aes(x=looic_1se_down, xend=looic_1se_up, y=predictor, yend=predictor), size=2, alpha=0.75) +
+	geom_point(aes(x=looic, y=predictor), size=5, alpha=1.0)  +
+	ggtitle("Model Comparisons") +
+	xlab("LOOIC (lower better)") +
 	ylab("") +
-	theme(axis.text.y=element_blank(),
-	      axis.ticks.y=element_blank()) +
-	xlim(0, 100)
+	theme_bw() +
+	theme(aspect.ratio = 1)
 
-ggsave("../plots/citations.png", plot=p, width=210, height=210*1.618, units="mm") 
+# Make the final combined plot
+plotlist <- c(list(p), plotlist)
+p <- wrap_plots(plotlist, nrow=1, ncol=10) +
+	plot_annotation(title=nice_title, theme = theme(plot.title = element_text(size = 18)))
+ggsave(paste("../plots/final_longer_citations_singlepred_interaction_comparison.png", sep=""), p, width=297*2, height=0.95*(2*210/5), units="mm")
