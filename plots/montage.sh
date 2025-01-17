17d8386a34145497f1d27513c747489408ea8427
Luke Maurits <luke@maurits.id.au>
2025-01-17 12:00:39 +0100

    Add ImageMagick montage script to combine qualifier and citation plots.

---
+++ plots/montage.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+montage -border 0 -geometry 7015 -tile 1x5 final_longer_group_identifier_singlepred_comparison.png \
+                            final_longer_crosscultural_singlepred_comparison.png \
+                            final_longer_culture_word_singlepred_comparison.png \
+                            final_longer_countries_info_given_singlepred_comparison.png \
+                            final_longer_citations_singlepred_interaction_comparison.png \
+			    final_longer_singlepred_mega_comparison.png
